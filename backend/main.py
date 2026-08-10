a35474d17941e682f4bb2adcd47f72ae023d71c9
ulacoder <nurtasulagat@gmail.com>
2026-08-10 19:51:48 +0500

    add CORS middleware to backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from gtts import gTTS
 import uuid
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/speak")
 def speak(text: str):
